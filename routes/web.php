3fe9af614e385bb08138b60226855a360551ea2c
sandeep9927 <sandeepsaini24398@gmail.com>
2020-08-27 20:16:06 +0530

    meditation

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('verify/{email}/{token}','Auth\RegisterController@verifyUser')->name('verify');
+
+Route::get('login/facebook', 'Auth\LoginController@redirectToProvider');
+Route::get('login/facebook/callback', 'Auth\LoginController@handleProviderCallback');
+Route::get('profile','UserProfileController@show');
+Route::get('profile/{id}/edit','UserProfileController@edit');
+Route::post('profile/update','UserProfileController@update');
+Route::view('edit','profiles.edit_profile');
